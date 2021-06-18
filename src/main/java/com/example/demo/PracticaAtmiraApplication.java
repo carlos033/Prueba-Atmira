1bc822352dea85d90dd4edc3686c97f985ba29b1
carlos033 <cdr8725@gmail.com>
2021-06-18 20:44:21 +0200

    Creación de modelos

---
+++ src/main/java/com/example/demo/PracticaAtmiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaAtmiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaAtmiraApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/modelo/DatosNasa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ck
+ */
+public class DatosNasa implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+    private int id;
+    private String name;
+    private String nasa_jpl_url;
+    private double absolute_magnitude_h;
+    
+        private boolean is_potentially_hazardous_asteroid;
+
+    
+}
