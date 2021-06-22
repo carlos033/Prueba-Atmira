b84f92192ce1fba785f03e4a4e0343be8fda1c61
carlos033 <cdr8725@gmail.com>
2021-06-22 22:02:28 +0200

    modificar metodo de obtenerDatosAsteroides

--- src/main/java/com/example/demo/servicio/Servicio.java
+++ src/main/java/com/example/demo/servicio/Servicio.java
@@ -12,6 +12,7 @@ import com.example.demo.modelo.DatosNasa;
 import com.example.demo.modelo.nasa.CloseApproachData;
 import com.example.demo.modelo.nasa.NearEarthObjectsDetails;
 import com.example.demo.servicioI.ServicioI;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -46,45 +47,39 @@ public class Servicio implements ServicioI {
     }
 
     @Override
-    public List<Asteroide> obtenerDatosAsteroides(String planeta) throws ExcepcionServicio {
+    public List<Asteroide> obtenerDatosAsteroides(String planeta) throws ExcepcionServicio, ParseException {
         List<Asteroide> listaObjetos = new ArrayList<>();
         LocalDate today = LocalDate.now();
         Map<String, List<NearEarthObjectsDetails>> listaAsteroides = new HashMap<>();
         DatosNasa datos = obtenerDatos();
         listaAsteroides = datos.getListaObjetos();
         for (Map.Entry<String, List<NearEarthObjectsDetails>> entry : listaAsteroides.entrySet()) {
-            for (NearEarthObjectsDetails item : entry.getValue()) {
-                if (item.isIs_potentially_hazardous_asteroid() == true) {
-                    for (CloseApproachData datosAproximacion : item.getDatosAproximacion()) {
-                        if (datosAproximacion) {
-                            
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
+            String date = entry.getKey();
+            Date formatted = new SimpleDateFormat("yyyy-mm-dd").parse(date);
+            LocalDate date1 = formatted.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            if (date1.isBefore(today.plusDays(7)) && date1.isAfter(today)) {
+                for (NearEarthObjectsDetails item : entry.getValue()) {
+                    if (item.isIs_potentially_hazardous_asteroid() == true) {
+                        for (CloseApproachData data : item.getDatosAproximacion()) {
+                            if (data.getCuerpoOrbital().compareToIgnoreCase(planeta) == 0) {
+                                double diametro = (item.getDiametro().getKilometros().getDiametroMax()
+                                        + item.getDiametro().getKilometros().getDiametroMin()) / 2;
+                                Asteroide asteroide = new Asteroide();
+                                asteroide.setFecha(date1);
+                                asteroide.setDiametro(diametro);
+                                asteroide.setNombre(item.getName());
+                                asteroide.setVelocidad(data.getVelocidades().getKmXH());
+                                asteroide.setPlaneta(planeta);
+                                listaObjetos.add(asteroide);
+                            }
                         }
-                    }
-                    item.getDatosAproximacion().stream().filter(data -> (data.getCuerpoOrbital().compareToIgnoreCase(planeta) == 0)).forEachOrdered((CloseApproachData data) -> {
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
-                        String date = data.getDatosAproximacion();
-                        LocalDate date1 = LocalDate.parse(date, formatter);
-                        if (date1.isBefore(today.plusDays(7)) && date1.isAfter(today)) {
-                            double diametro = (item.getDiametro().getKilometros().getDiametroMax()
-                                    + item.getDiametro().getKilometros().getDiametroMin()) / 2;
-                            Asteroide asteroide = new Asteroide();
-                            asteroide.setFecha(date1);
-                            asteroide.setDiametro(diametro);
-                            asteroide.setNombre(item.getName());
-                            asteroide.setVelocidad(data.getVelocidades().getKmXH());
-                            asteroide.setPlaneta(planeta);
-                            listaObjetos.add(asteroide);
-                        }
-                    });
-                    if (listaObjetos.isEmpty()) {
-                        throw new ExcepcionServicio("El planeta no existe o no tiene asteroides orbitando en los proximos 7 dias");
                     }
                 }
             }
         }
-        listaObjetos.sort((o1, o2) -> Double.compare(o2.getDiametro(), o1.getDiametro()));
-        for (int i = 3; i <= listaObjetos.size(); ++i) {
-            listaObjetos.remove(i);
+        if (listaObjetos.isEmpty()) {
+            throw new ExcepcionServicio("El planeta no existe o no tiene asteroides orbitando en los proximos 7 dias");
         }
         return listaObjetos;
     }
